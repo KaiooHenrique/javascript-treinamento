81bf3c3786c4fb216bc62fb39b559792280e35da
KaiooHenrique <kaiohenriquepereira09@gmail.com>
2022-11-15 00:10:40 -0300

    Aplicação de Funcões no codigo

--- exercicio3.js
+++ exercicio3.js
@@ -1,15 +1,22 @@
 
+function aplicarDesconto(valor, desconto){
+    return (valor - (valor * (desconto / 100)));
+}
+function aaplicarAcrecimo(valor, acrecimo){
+    return (valor + (valor * (acrecimo / 100)));
+}
+
 const precoEtiqueta = 100
 const formaDePagamento = 4;
 
 if(formaDePagamento === 1){
-    console.log (precoEtiqueta - (precoEtiqueta * 0.1));
+    console.log (aplicarDesconto(precoEtiqueta, 10));
 }else if(formaDePagamento === 2){
-    console.log (precoEtiqueta - (precoEtiqueta * 0.15));
+    console.log (aplicarDesconto(precoEtiqueta, 15));
 }else if(formaDePagamento === 3){
     console.log(precoEtiqueta); 
 }else{(formaDePagamento === 4)
-    console.log(precoEtiqueta + (precoEtiqueta * 0.1));
+    console.log(aaplicarAcrecimo(precoEtiqueta, 10));
 }
 
 
