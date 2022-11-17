4778e202397dbc342da94f527193d71bef016c9e
KaiooHenrique <kaiohenriquepereira09@gmail.com>
2022-11-16 22:57:29 -0300

    aplicando function

--- exercicio2.js
+++ exercicio2.js
@@ -1,26 +1,35 @@
 
+function calcularImc(peso, altura){
+   return imc = peso / (altura * altura);
+}
+function classificarIms(imc){
+    if (imc < 18.5) {
+        return ('Abaixo do Peso');
+    } else if (imc >= 18.5 && imc <= 25) {
+        return ('Peso Normal');
+    
+    } else if (imc >= 25.1 && imc <= 30) {
+        return ('Acima do Peso');
+    
+    } else if (imc >= 30.1 && imc <= 40) {
+        return ('Obeso');
 
-
-const peso = 60;
-const altura = 1.55;
-
-imc = peso / (altura * altura);
-console.log(imc.toFixed(1));
-
-if (imc < 18.5) {
-    console.log('Abaixo do Peso');
-} else if (imc >= 18.5 && imc <= 25) {
-    console.log('Peso Normal');
-
-} else if (imc >= 25.1 && imc <= 30) {
-    console.log('Acima do Peso');
-
-} else if (imc >= 30.1 && imc <= 40) {
-    console.log('Obeso');
-
-} else {
-    (imc > 40)
-    console.log('Obesidade Grave');
+    } else {
+        (imc > 40)
+        return ('Obesidade Grave');
+    }
+    
+    
 }
 
+
+function main(){
+    const peso = 60;
+const altura = 1.55;
+
+const imc = calcularImc(peso, altura);
+console.log (classificarIms(imc));
+}
+
+main();
 
